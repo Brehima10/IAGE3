d7545dc2485655eec61146ad246df6ceeb86044a
unknown <Brehima>
2021-01-24 23:29:26 +0000

    DEVOIR DE JAVA

---
+++ DEVOIR/GestionEtudiants/src/gestionetudiant/inscriptionController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionetudiant;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+/**
+ *
+ * @author HP
+ */
+public class inscriptionController {
+
+    @FXML
+    private ComboBox<?> textclasse;
+    
+}
